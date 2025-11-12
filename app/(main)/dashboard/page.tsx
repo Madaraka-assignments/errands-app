176f61fb7829176a73f67db43d5a649977121bea
VictorDdsolutions <victor.madaraka@ddsolutions.tech>
2025-11-12 16:49:32 +0300

    [upd] show task creation form

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   {isError && <p>Failed to load tasks</p>}
 
       
-  {!isLoading && !isError && data ?
+  {!isLoading && !isError && data?.tasks && data.tasks.length > 0 ?
     <div className="flex min-h-svh w-full  p-6 md:p-10">
     <div className="w-full max-w-ld">
       
