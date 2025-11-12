5d24567e2b1ce28fe6238a9ac3f3ff2da51b3d76
VictorDdsolutions <victor.madaraka@ddsolutions.tech>
2025-11-12 10:18:53 +0300

    [feat] auth types

---
+++ types/auth.ts
@@ -0,0 +1,38 @@
+export interface RegisterRequest {
+    first_name:   string;
+    last_name:    string;
+    phone_number: string;
+    email:        string;
+    password:     string;
+}
+
+
+export interface RegisterResponse {
+    id:              number;
+    customer_number: string;
+    user:            User;
+}
+
+export interface User {
+    id:              string;
+    first_name:      string;
+    last_name:       string;
+    email:           string;
+    phone_number:    string;
+    customer_number: string;
+    staff_id:        null;
+    roles:           null;
+}
+
+
+export interface LoginRequest {
+    email:        string;
+    password:     string;
+}
+
+
+export interface LoginResponse {
+    refresh: string;
+    access:  string;
+    user:    User;
+}
